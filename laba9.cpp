de4e3d10b21b06b5ece9d5a5592aa5000c57df3b
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-01-15 09:57:07 +0300

    Create laba9.cpp

---
+++ laba9.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main(void)
+{
+	FILE *fpin;
+	FILE *fpout;
+
+	int N = l;
+	int i = 1;
+	char ptr;
+
+	fpin = fopen("test.txt", "r");
+	fpout = fopen("result.txt", "w");
+
+	while ((ptr = fgetc(fpin)) != EOF)
+	{
+		if (ptr == '_' || ptr == '.' || ptr == ',')
+			i++;
+
+		if (i == N){
+			do
+				ptr = fgetc(fpin);
+			while (ptr != '_');
+			i++;
+		}
+
+		if (ptr == '\n')
+			i = 1;
+		fprintf(fpout, "%c", ptr);
+	}
+
+	fclose(fpin);
+	fclose(fpout);
