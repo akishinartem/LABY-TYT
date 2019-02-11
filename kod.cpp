f163b58de3622a915208de25cec3e81c8d06746a
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-02-11 23:29:19 +0300

    Add files via upload

---
+++ kod.cpp
@@ -0,0 +1,44 @@
+#include "string.h"
+#include <stdio.h> 
+#include <stdlib.h> 
+#define YES 1
+#define NO 0
+#define MAXLEN 1000 
+#define NLINES 100
+int main(void)
+{
+	system("chcp 1251");
+	system("cls");
+	char str[32];
+	FILE *f;
+	char* strs;
+	int count = 0, i, j;
+	if ((f = fopen("test.txt", "r")) == NULL)
+	{
+		return;
+	}
+	strs = (char*)malloc(sizeof(char)*MAXLEN*NLINES);
+	while (!feof(f))
+	{
+		fgets(strs + count++ * MAXLEN, MAXLEN, f);
+	}
+	fclose(f);
+	for (i = 1; i < count; i++)
+	{
+		for (j = 0; j < count - i; j++)
+		{
+			if (strcmp(strs + j * MAXLEN, strs + (j + 1) * MAXLEN) > 0)
+			{
+				strcpy(str, strs + j * MAXLEN);
+				strcpy(strs + j * MAXLEN, strs + (j + 1) * MAXLEN);
+				strcpy(strs + (j + 1) * MAXLEN, str);
+			}
+		}
+	}
+	for (i = 0; i < count; i++)
+	{
+		printf("%s", strs + i * MAXLEN);
+	}
+	system("pause");
+	return 0;
+}
