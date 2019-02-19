184be6341fccb4a34977e22fa1bab47d5b1810c6
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-02-20 00:46:15 +0300

    Update and rename kok.cpp to Laba11.cpp

--- Laba11.cpp
+++ Laba11.cpp
@@ -7,30 +7,26 @@
 
 int main(void)
 {
-	HANDLE hStdout; // дескриптор консольного окна
-	FILE *fp; // входной файл
-	char line[MAXLEN]; // строчка из файла
-	char word[MAXLEN];// слово
+	HANDLE hStdout;
+	FILE *fp;
+	char line[MAXLEN];
+	char word[MAXLEN];
 	char mass[30][100] = { "auto","continue","break","case","char","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","while" };
 	int i,j;
 	int flag1;
-	int flag; // флаг слово/разделитель
-	int prev_flag; // предыдущее значение флага
-	char *ptr; // указатель на строчку
-	WORD foregroundColor; // цвет символа
-	WORD backgroundColor; // цвет фона
-	WORD textAttribute; // атрибут текста - цвет символа и фона
-
-	// Получить стандартный дескриптор
+	int flag;
+	int prev_flag;
+	char *ptr;
+	WORD foregroundColor;
+	WORD backgroundColor;
+	WORD textAttribute;
 	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-	// определить цвета символов, разделителей и фона
 	foregroundColor = FOREGROUND_INTENSITY | FOREGROUND_BLUE;
 	backgroundColor = BACKGROUND_INTENSITY | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED;
-	// открыть файл для чтения
 	fp = fopen("test.txt", "rt");
 	if (fp == NULL)
-		return 1; // ошибка при открытии файла
-
+		return 1;
+	
 	while (!feof(fp))
 	{
 		ptr = fgets(line, MAXLEN, fp);
@@ -41,21 +37,21 @@ int main(void)
 		word[0] = '\0';
 		while (*ptr != 0)
 		{
-			prev_flag = flag; // запомнить флаг
+			prev_flag = flag;
 			if (*ptr == ' ' || *ptr == ',' || *ptr == '.' || *ptr == '\n' || *ptr == ';' || *ptr == '\t')
 				flag = NOT_THE_LETTER;
 			else
 				flag = LETTER;
 			if (flag != prev_flag)
 			{
-				word[i] = '\0'; // закрыть слово
+				word[i] = '\0';
 				if (prev_flag)
 				{
 					for (j = 0; j < 30; j++)
 					{
 						if (!strcmp(word, mass[j]))
 						{
-							flag1 = 1;// Найдено слово в массиве
+							flag1 = 1;
 						}
 					}
 				}
@@ -69,20 +65,20 @@ int main(void)
 				{
 					SetConsoleTextAttribute(hStdout, 7);
 				}
-				printf("%s", word); // вывести слово
-				i = 0; // начать новое слово
+				printf("%s", word);
+				i = 0;
 			}
-			word[i++] = *ptr++; // запомнить символ
+			word[i++] = *ptr++;
 		}
 	}
 	if (i != 0)
 	{
-		word[i] = '\0'; // закрыть слово
+		word[i] = '\0';
 		for (j = 0; j < 30; j++)
 		{
 			if (!strcmp(word, mass[j]))
 			{
-				flag1 = 1;// Найдено слово в массиве
+				flag1 = 1;
 			}
 		}
 		if (flag1)
@@ -95,10 +91,9 @@ int main(void)
 		{
 			SetConsoleTextAttribute(hStdout, 7);
 		}
-		printf("%s", word); // вывести слово
+		printf("%s", word);
 	}
 	printf("\n");
-	// белые символы, черный фон
 	SetConsoleTextAttribute(hStdout, 7);
 	system("pause");
 	return 0;
